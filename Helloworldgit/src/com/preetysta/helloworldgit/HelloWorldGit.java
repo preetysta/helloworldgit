c37ed2b449dfb07ce842eb382b6c297ff1cf80ac
preetysta <preetysta@gmail.com>
2017-12-26 15:28:25 -0500

    committed helloworldgit project

---
+++ Helloworldgit/src/com/preetysta/helloworldgit/HelloWorldGit.java
@@ -0,0 +1,11 @@
+package com.preetysta.helloworldgit;
+
+public class HelloWorldGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World Git!");
+
+	}
+
+}
