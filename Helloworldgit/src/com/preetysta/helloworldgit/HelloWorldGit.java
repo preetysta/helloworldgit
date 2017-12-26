a21ef53bd95757a83ecab441163c374c4269d8cc
preetysta <preetysta@gmail.com>
2017-12-26 15:30:08 -0500

    added a comment

--- Helloworldgit/src/com/preetysta/helloworldgit/HelloWorldGit.java
+++ Helloworldgit/src/com/preetysta/helloworldgit/HelloWorldGit.java
@@ -3,7 +3,7 @@ package com.preetysta.helloworldgit;
 public class HelloWorldGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// a greeting
 		System.out.println("Hello World Git!");
 
 	}
